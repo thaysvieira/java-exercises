3f3617a212bfb5e4f369fc3a7eeeb3ab62fa9aa1
Thays Vieira <thaysvieira39@gmail.com>
2026-04-05 20:27:33 +0200

    Inheritance exercises done

--- src/main/java/com/amigoscode/_3_oop/_2_inheritance/ElectricCar.java
+++ src/main/java/com/amigoscode/_3_oop/_2_inheritance/ElectricCar.java
@@ -14,47 +14,74 @@ package com.amigoscode._3_oop._2_inheritance;
  */
 
 // TODO: 1 - Make this class extend Vehicle.
-public class ElectricCar {
+public class ElectricCar extends Vehicle {
 
     // TODO: 2 - Add a private field:
     //   - batteryLevel (int) representing percentage from 0 to 100
+    private int batteryLevel;
 
+    public ElectricCar(String make, String model, int year) {
+        super(make, model, year);
+    }
 
     // TODO: 3 - Create a constructor that takes make, model, year, and batteryLevel.
     //   Call super(make, model, year) first, then set the batteryLevel field.
 
+    public ElectricCar(String make, String model, int year, int batteryLevel) {
+        super(make, model, year);
+        this.batteryLevel = batteryLevel;
+    }
 
     // TODO: 4 - Override the start() method to print:
     //   "<make> <model> electric motor starting silently..."
     //   Use the @Override annotation. Access make and model from
     //   the parent class (they are protected).
 
+    @Override
+    public void start() {
+        System.out.println(this.make+" "+this.model+" electric motor starting silently...");
+    }
+
 
     // TODO: 5 - Add a charge(int percent) method specific to ElectricCar.
     //   - Add percent to batteryLevel
     //   - Cap batteryLevel at 100 (use Math.min)
     //   - Print: "Charging... Battery now at <batteryLevel>%"
-
+    public void charge(int percent){
+        System.out.println("Charging... Battery now at "+this.batteryLevel+"%");
+    }
 
     // TODO: 6 - Add a getBatteryStatus() method that returns a String:
     //   "Battery: <batteryLevel>%"
     //   Also override toString() to return:
     //   "ElectricCar{make='XXX', model='XXX', year=XXX, batteryLevel=XXX%}"
+    public String getBatteryStatus(){
+        return "Battery: "+this.batteryLevel+"%";
+    }
 
+    @Override
+    public String toString() {
+        return "ElectricCar{" +
+                "batteryLevel=" + batteryLevel +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
 
     public static void main(String[] args) {
         // Uncomment and test after completing the TODOs:
-        // ElectricCar tesla = new ElectricCar("Tesla", "Model 3", 2024, 85);
-        // System.out.println(tesla);
-        // tesla.start();                      // overridden — silent start
-        // System.out.println(tesla.getInfo()); // inherited from Vehicle
-        // System.out.println(tesla.getBatteryStatus());
+         ElectricCar tesla = new ElectricCar("Tesla", "Model 3", 2024, 85);
+         System.out.println(tesla);
+         tesla.start();                      // overridden — silent start
+         System.out.println(tesla.getInfo()); // inherited from Vehicle
+         System.out.println(tesla.getBatteryStatus());
         //
-        // tesla.charge(20);
-        // System.out.println(tesla.getBatteryStatus());
+         tesla.charge(20);
+         System.out.println(tesla.getBatteryStatus());
         //
         // // Polymorphism: an ElectricCar IS-A Vehicle
-        // Vehicle vehicle = new ElectricCar("Rivian", "R1T", 2025, 60);
-        // vehicle.start(); // calls ElectricCar's overridden version
+         Vehicle vehicle = new ElectricCar("Rivian", "R1T", 2025, 60);
+         vehicle.start(); // calls ElectricCar's overridden version
     }
 }
