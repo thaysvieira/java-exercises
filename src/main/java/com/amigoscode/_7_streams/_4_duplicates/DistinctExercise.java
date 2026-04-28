87daf9ab39ee60e9eeccce01d02a413fca49b91f
Thays Vieira <thaysvieira39@gmail.com>
2026-04-28 14:15:52 +0200

    Done distinct

--- src/main/java/com/amigoscode/_7_streams/_4_duplicates/DistinctExercise.java
+++ src/main/java/com/amigoscode/_7_streams/_4_duplicates/DistinctExercise.java
@@ -1,9 +1,8 @@
 package com.amigoscode._7_streams._4_duplicates;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
+
 
 /**
  * Exercise: Removing Duplicates
@@ -29,32 +28,38 @@ public class DistinctExercise {
 
         // TODO: 1 - Remove duplicates from 'numbersWithDuplicates' using distinct()
         //           Print the resulting unique numbers
-
+        List<Integer> distinct = numbersWithDuplicates.stream().distinct().toList();
+        System.out.println(distinct);
 
         // TODO: 2 - Remove duplicates from 'mixedCaseNames' in a case-insensitive manner
         //           Map all names to lowercase first, then use distinct()
         //           Print the resulting unique names
-
+        List<String> names = mixedCaseNames.stream().distinct().map(String::toLowerCase).toList();
+        System.out.println(names);
 
         // TODO: 3 - Collect 'fruits' to a Set instead of using distinct()
         //           This automatically removes duplicates
         //           Print the resulting set
-
+        Set<String> collect = new HashSet<>(fruits);
+        System.out.println(collect);
 
         // TODO: 4 - Count the number of distinct elements in 'numbersWithDuplicates'
         //           Use distinct() followed by count()
         //           Print the count
-
+        long count = numbersWithDuplicates.stream().distinct().count();
+        System.out.println(count);
 
         // TODO: 5 - Remove duplicate Person objects from 'people'
         //           Since Person is a record, equals/hashCode are auto-generated
         //           Use distinct() and print each unique person
-
+        List<Person> personList = people.stream().distinct().toList();
+        System.out.println(personList);
 
         // TODO: 6 - Remove duplicates from 'fruits' with custom logic using a TreeSet
         //           collector that ignores case (use String.CASE_INSENSITIVE_ORDER)
         //           Hint: Collectors.toCollection(() -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER))
         //           Print the result
-
+        TreeSet<String> stringTreeSet = fruits.stream().distinct().collect(Collectors.toCollection(() -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER)));
+        System.out.println(stringTreeSet);
     }
 }
